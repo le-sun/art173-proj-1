6d7d5840bd5f1720dd04b6c1e8d50d41a394dd78
Leo <exampletestaccount1@gmail.com>
2018-09-18 00:35:30 -0700

    testing sound loading

--- src/sketch.js
+++ src/sketch.js
@@ -1,8 +1,7 @@
 var numWhiteKeys = 7;
 var numBlackKeys = 5;
 var keyLoc = 10;
-var naturalKeyNames = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
-var sharpKeyNames = ['A', 'C', 'D', 'F', 'G']
+var naturalKeyNames = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 var sounds = [];
 var keys = [];
 
@@ -25,12 +24,12 @@ class Key {
 }
 
 function preload() {
-  console.log('1')
-  loadSound('assets/reg-A.mp3')
-  loadSound(String('assets/reg-' + naturalKeyNames[0] + '.mp3'));
-  console.log('hmm')
   for (let i = 0; i < naturalKeyNames.length; i++) {
     sounds.push(loadSound(String('assets/reg-' + naturalKeyNames[i] + '.mp3')));
+
+    if (i != 2 && i != 6) {
+      sounds.push(loadSound(String('assets/reg-' + naturalKeyNames[i] + '#.mp3')));
+    }
   }
   console.log(sounds)
 }
@@ -44,29 +43,6 @@ function setup() {
       new Key('100px', '31px', '209px', String(53 + (i*51)) + 'px', 2, i + 7);
     }
   }
-
-  // for (let i = 0; i < numWhiteKeys; i++) {
-  //   let newKey = createDiv();
-  //   newKey.style('position', 'absolute');
-  //   newKey.style('height', '200px')
-  //   newKey.style('width', '50px')
-  //   newKey.style('top', '209px')
-  //   newKey.style('left', String(19 + (i*51)) + 'px')
-  //   newKey.style('z-index', 1)
-  //   keys.push(newKey);
-  // }
-
-  // for (let i = 0; i < numWhiteKeys; i++) {
-  //   let newKey = createDiv();
-  //   newKey.style('position', 'absolute');
-  //   newKey.style('height', '100px')
-  //   newKey.style('width', '31px')
-  //   newKey.style('top', '209px')
-  //   newKey.style('left', String(53 + (i*51)) + 'px')
-  //   newKey.style('z-index', 2)
-  //   keys.push(newKey);
-  // }
-  let x = new Key('200px', '50px', '50px', '209px', '1');
 }
 
 function draw() {
@@ -84,6 +60,6 @@ function draw() {
   }
 }
 
-function playSound() {
-
+function playSound(soundIndex) {
+  sounds[i].play()
 }
